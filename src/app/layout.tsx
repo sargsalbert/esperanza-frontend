7cb369da56be3bf67eb873920fcefad633cda1bd
sargsalbert <sargsyan.abet@gmail.com>
2025-04-11 16:16:24 +0400

    test safari

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next';
 import { Montserrat } from 'next/font/google';
-import './globals.css';
 import Header from '@/components/shared/Header';
 import { Footer } from '@/components/shared/Footer';
+import './globals.css';
 
 const montserrat = Montserrat({
   subsets: ['latin'],
