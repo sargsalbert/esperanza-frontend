e4cf9366997b59c738d7083e1434caab424f6a31
sargsalbert <sargsyan.abet@gmail.com>
2025-04-11 18:54:52 +0400

    test

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,14 +1,12 @@
-import postcssPresetEnv from 'postcss-preset-env';
-
 const config = {
   plugins: {
     '@tailwindcss/postcss': {},
-    'postcss-preset-env': postcssPresetEnv({
-      stage: 1, // Optional: you can set stage based on your preference
+    'postcss-preset-env': {
+      stage: 3, // Optional: you can set stage based on your preference
       features: {
         'nesting-rules': true, // Optional: enable CSS nesting
       },
-    }),
+    },
   },
 };
 
