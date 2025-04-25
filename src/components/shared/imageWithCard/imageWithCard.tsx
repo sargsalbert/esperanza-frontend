31281a63c8499f59903654a472ca34fc1f43a3fa
sargsalbert <sargsyan.abet@gmail.com>
2025-04-26 00:13:55 +0400

    all pages

--- src/components/shared/imageWithCard/imageWithCard.tsx
+++ src/components/shared/imageWithCard/imageWithCard.tsx
@@ -65,7 +65,9 @@ const ImageWithCard = ({
     <>
       {uiType === 'collapse' && (
         <div onClick={handleCollapse} className='bg-gray-100 px-8'>
-          <h2 className='cursor-pointer border-b-2 border-gray-200 py-4 text-center text-[18px]/[22px] font-semibold text-gray-800 uppercase md:text-[20px]/[24px] lg:hidden'>
+          <h2
+            className={`cursor-pointer ${collapseType ? 'border-b-2' : 'border-b-0'} border-gray-200 py-4 text-center text-[18px]/[22px] font-semibold text-gray-800 uppercase md:text-[20px]/[24px] lg:hidden`}
+          >
             {title}
           </h2>
         </div>
