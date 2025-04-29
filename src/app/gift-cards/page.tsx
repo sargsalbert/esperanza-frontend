0a6e22d841461dbfe0216a081c0b698cb0ccf1e2
sargsalbert <sargsyan.abet@gmail.com>
2025-04-29 16:42:06 +0400

    minor change

--- src/app/gift-cards/page.tsx
+++ src/app/gift-cards/page.tsx
@@ -74,7 +74,7 @@ export default function GiftCards() {
         <div className='flex justify-center md:justify-end'>
           <button
             // onClick={primaryButton.onClick}
-            className={`h-9 min-w-63 cursor-pointer rounded-full border-2 border-gray-800 bg-gray-800 px-5 py-1 text-sm font-semibold text-gray-50 md:h-10.5 md:min-w-38 md:text-base 2xl:h-12.5 2xl:min-w-60 2xl:border-3 2xl:text-lg`}
+            className={`h-9 min-w-63 cursor-pointer rounded-full border-2 border-gray-800 bg-gray-800 px-5 py-1 text-sm font-semibold text-gray-50 lg:h-10.5 lg:min-w-38 lg:text-base 2xl:h-12.5 2xl:min-w-60 2xl:border-3 2xl:text-lg`}
           >
             Purchase
           </button>
