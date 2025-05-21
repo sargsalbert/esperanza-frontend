f461a718bb91c37e8c38da7fb1f5e6db3098b9bb
sargsalbert <sargsyan.abet@gmail.com>
2025-05-21 22:27:46 +0400

    minor change

--- src/components/shared/Hamburger.tsx
+++ src/components/shared/Hamburger.tsx
@@ -7,7 +7,7 @@ const Hamburger = ({ isOpen }: { isOpen: boolean }) => {
         className={`absolute h-0.5 rounded-xl bg-gray-800 transition-all duration-300 ease-in-out ${
           isOpen
             ? 'w-[22px] rotate-45 lg:w-[30px] 2xl:w-[36px]'
-            : 'w-[22px] -translate-y-1.5 rotate-0 lg:w-[30px] lg:-translate-y-2.5 2xl:w-[36px] 2xl:-translate-y-2.5'
+            : 'w-[22px] -translate-y-1.5 rotate-0 lg:w-[30px] lg:-translate-y-2 2xl:w-[36px] 2xl:-translate-y-2.5'
         }`}
       />
       <span
@@ -21,7 +21,7 @@ const Hamburger = ({ isOpen }: { isOpen: boolean }) => {
         className={`absolute h-0.5 rounded-xl bg-gray-800 transition-all duration-300 ease-in-out ${
           isOpen
             ? 'w-0 opacity-0'
-            : 'w-[10px] translate-y-1.5 lg:w-[12px] lg:translate-y-2.5 2xl:w-[16px] 2xl:translate-y-2.5'
+            : 'w-[10px] translate-y-1.5 lg:w-[12px] lg:translate-y-2 2xl:w-[16px] 2xl:translate-y-2.5'
         }`}
       />
     </>
