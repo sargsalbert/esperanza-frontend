c3fe507ca15b4c3fb57e4fd18cf0b9c2b1425a75
sargsalbert <sargsyan.abet@gmail.com>
2025-05-21 21:41:35 +0400

    test

--- src/components/shared/Hamburger3.tsx
+++ src/components/shared/Hamburger3.tsx
@@ -9,19 +9,19 @@ const Hamburger3 = ({ isOpen }: { isOpen: boolean }) => {
       {/* Top Line - becomes part of X */}
       <span
         className={`absolute h-px rounded-md bg-gray-800 transition-all duration-300 ease-in-out ${
-          isOpen ? 'w-[18px] rotate-45' : 'w-[18px] -translate-y-1.5 rotate-0'
+          isOpen ? 'w-[24px] rotate-45' : 'w-[24px] -translate-y-1.5 rotate-0'
         }`}
       />
       {/* Middle Line - becomes other part of X */}
       <span
         className={`absolute h-px rounded-md bg-gray-800 transition-all duration-300 ease-in-out ${
-          isOpen ? 'w-[18px] -rotate-45' : 'w-[12px] translate-y-0 rotate-0'
+          isOpen ? 'w-[24px] -rotate-45' : 'w-[18px] translate-y-0 rotate-0'
         }`}
       />
       {/* Bottom Line - disappears when open */}
       <span
         className={`absolute h-px rounded-md bg-gray-800 transition-all duration-300 ease-in-out ${
-          isOpen ? 'w-0 opacity-0' : 'w-[8px] translate-y-1.5'
+          isOpen ? 'w-0 opacity-0' : 'w-[12px] translate-y-1.5'
         }`}
       />
     </button>
