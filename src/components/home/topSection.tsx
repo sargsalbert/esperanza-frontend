6b6da32b0ea992ee1b2d4092c048e45670767852
sargsalbert <sargsyan.abet@gmail.com>
2025-05-28 17:49:20 +0400

    minor fix

--- src/components/home/topSection.tsx
+++ src/components/home/topSection.tsx
@@ -15,7 +15,7 @@ const TopSection = ({ url, title }: TopSectionProps) => {
         priority
         className='object-cover'
       />
-      <div className='relative z-1 flex h-[40%] w-full items-end justify-center bg-gradient-to-t from-black via-black/70 to-transparent pb-[72px] lg:pb-[155px]'>
+      <div className='relative z-1 flex h-[40%] w-full items-end justify-center bg-gradient-to-t from-black via-black/70 to-transparent pb-[72px] lg:pb-[120px] 2xl:pb-[155px]'>
         {title && (
           <h1 className='px-5 text-center text-[22px] font-semibold text-gray-50 uppercase sm:px-7.5 sm:text-[30px] lg:px-15 lg:text-[40px]'>
             {title}
