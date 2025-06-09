1c79efb4f8cf12a305b359863b5e5bdc6d30eb9d
sargsalbert <sargsyan.abet@gmail.com>
2025-06-09 13:52:49 +0400

    test

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,12 +1,16 @@
 const config = {
+  // plugins: {
+  //   '@tailwindcss/postcss': {},
+  //   'postcss-preset-env': {
+  //     stage: 0, // Optional: you can set stage based on your preference
+  //     features: {
+  //       'nesting-rules': true, // Optional: enable CSS nesting
+  //     },
+  //   },
+  // },
+
   plugins: {
-    '@tailwindcss/postcss': {},
-    'postcss-preset-env': {
-      stage: 0, // Optional: you can set stage based on your preference
-      features: {
-        'nesting-rules': true, // Optional: enable CSS nesting
-      },
-    },
+    '@tailwindcss/postcss': {}, // only if you're explicitly using it
   },
 };
 
