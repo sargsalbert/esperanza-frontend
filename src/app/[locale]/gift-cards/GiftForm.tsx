6bc977b61e28d58d58ad5d02e665bb7a03f4bce0
sargsalbert <sargsyan.abet@gmail.com>
2025-06-27 18:01:17 +0400

    minor change

--- src/app/[locale]/gift-cards/GiftForm.tsx
+++ src/app/[locale]/gift-cards/GiftForm.tsx
@@ -155,7 +155,7 @@ export default function GiftForm({ data, locale }: GiftFormProps) {
                 <div className='flex justify-center lg:justify-end'>
                   <button
                     type='submit'
-                    disabled={isSubmitting || !isValid}
+                    disabled={isSubmitting}
                     className={`h-9 min-w-63 cursor-pointer rounded-full border-2 border-gray-800 bg-gray-800 px-5 py-1 text-sm font-semibold text-gray-50 md:h-10.5 lg:min-w-38 lg:text-base 2xl:h-12.5 2xl:min-w-60 2xl:border-3 2xl:text-lg`}
                   >
                     {data.giftCard?.formButtonText}
