4509ba22c9c95db02f1fe4eb90c6f384b55eaad5
sargsalbert <sargsyan.abet@gmail.com>
2025-06-29 19:56:45 +0400

    test fade component

--- src/components/shared/FadeInOnView.tsx
+++ src/components/shared/FadeInOnView.tsx
@@ -22,7 +22,7 @@ const FadeInOnView: React.FC<FadeInOnViewProps> = ({
     <div
       ref={ref}
       style={{ transitionDelay: `${delay}ms` }}
-      className={`relative z-1 transform transition-all duration-800 ease-in-out ${
+      className={`transform transition-all duration-800 ease-in-out ${
         inView ? 'translate-y-0 opacity-100' : 'translate-y-4 opacity-0'
       } ${className}`}
     >
