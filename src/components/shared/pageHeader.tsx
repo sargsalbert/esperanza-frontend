0baa75f31c129c4bf5d01e8a7e0a6074fed7c24a
sargsalbert <sargsyan.abet@gmail.com>
2025-07-06 01:33:47 +0400

    minor change

--- src/components/shared/pageHeader.tsx
+++ src/components/shared/pageHeader.tsx
@@ -17,7 +17,7 @@ const PageHeader: FC<PageHeaderProps> = ({ heroImage, title = '' }) => {
     <div className='relative mb-7.5 flex h-[calc(var(--vh,1vh)*100-84px)] w-full md:h-[calc(var(--vh,1vh)*100-110px)] lg:mb-10 xl:mb-15 2xl:h-[calc(var(--vh,1vh)*100-123px)]'>
       <div className='relative flex h-full w-full overflow-hidden'>
         {heroImage && <StrapiImage image={heroImage} className='absolute' />}
-        <div className='relative flex h-full w-full items-center justify-center bg-[linear-gradient(0deg,_rgba(0,0,0,0.2)_0%,_rgba(255,255,255,0)_100%)]'>
+        <div className='relative flex h-full w-full items-center justify-center bg-[linear-gradient(0deg,_rgba(0,0,0,0.4)_0%,_rgba(255,255,255,0)_100%)]'>
           {title && (
             <h1 className='p-5 text-center text-[26px] font-semibold text-gray-50 uppercase sm:px-7.5 sm:text-[32px] md:text-[38px] lg:px-15 xl:text-[44px] 2xl:text-[50px]'>
               {title}
