4b5cee1657fb9178229dce26043adeb0f6c414b1
sargsyan.abet@gmail.com <sargsalbert>
2026-02-07 00:48:31 +0400

    revert test

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -82,7 +82,6 @@ export function Footer({ global }: FooterProps) {
           <Script
             src='https://widgets.experience-hotel.com/front/widget/widget-newsletter/bootstrap.js?widget_id=489'
             strategy='afterInteractive'
-            crossOrigin='anonymous'
           />
           {/* end widgets experience hotel */}
 
