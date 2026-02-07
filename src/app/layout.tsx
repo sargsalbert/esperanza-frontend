de5673ac2b3a2f13622d392e39af0c79d90d0db1
sargsyan.abet@gmail.com <sargsalbert>
2026-02-07 16:55:18 +0400

    revert test disable gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
-// import Script from 'next/script';
-// import { GoogleAnalytics } from '@next/third-parties/google';
-// import { FB_PIXEL_ID } from '@/lib/fbpixel/fbpixel';
+import Script from 'next/script';
+import { GoogleAnalytics } from '@next/third-parties/google';
+import { FB_PIXEL_ID } from '@/lib/fbpixel/fbpixel';
 
 export default function RootLayout({
   children,
@@ -12,7 +12,7 @@ export default function RootLayout({
     <html suppressHydrationWarning>
       <head>
         {/* Google Tag Manager (HEAD script) */}
-        {/* <Script id='gtm-head' strategy='afterInteractive'>
+        <Script id='gtm-head' strategy='afterInteractive'>
           {`
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -20,10 +20,10 @@ export default function RootLayout({
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-PM8J9TMP');
           `}
-        </Script> */}
+        </Script>
 
         {/* Google Ads (gtag.js) */}
-        {/* <Script
+        <Script
           src='https://www.googletagmanager.com/gtag/js?id=AW-17528734611'
           strategy='afterInteractive'
         />
@@ -34,10 +34,10 @@ export default function RootLayout({
             gtag('js', new Date());
             gtag('config', 'AW-17528734611');
           `}
-        </Script> */}
+        </Script>
 
         {/* Facebook Pixel */}
-        {/* {FB_PIXEL_ID && (
+        {FB_PIXEL_ID && (
           <Script
             id='facebook-pixel'
             strategy='afterInteractive'
@@ -56,27 +56,27 @@ export default function RootLayout({
               `,
             }}
           />
-        )} */}
+        )}
       </head>
 
       <body suppressHydrationWarning>
         {/* GTM noscript (MUST be the first element in <body>) */}
-        {/* <noscript
+        <noscript
           dangerouslySetInnerHTML={{
             __html: `
               <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PM8J9TMP"
               height="0" width="0" style="display:none;visibility:hidden"></iframe>
             `,
           }}
-        /> */}
+        />
 
         {children}
 
         {/* Google Analytics */}
-        {/* <GoogleAnalytics gaId='G-CR699031L4' /> */}
+        <GoogleAnalytics gaId='G-CR699031L4' />
 
         {/* FB Pixel fallback */}
-        {/* {FB_PIXEL_ID && (
+        {FB_PIXEL_ID && (
           <noscript>
             <img
               height='1'
@@ -86,7 +86,7 @@ export default function RootLayout({
               alt=''
             />
           </noscript>
-        )} */}
+        )}
       </body>
     </html>
   );
